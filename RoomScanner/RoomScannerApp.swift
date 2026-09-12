2159e753dab3ab638cd63fb4d55d692f9bf100c0
Walter <dynamic.walter@gmail.com>
2026-09-12 07:35:44 -0400

    Initial Commit

---
+++ RoomScanner/RoomScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomScannerApp.swift
+//  RoomScanner
+//
+//  Created by Walter Hsieh on 9/12/26.
+//
+
+import SwiftUI
+
+@main
+struct RoomScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
